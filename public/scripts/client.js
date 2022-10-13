acac831c7e0e82eee3e817e8a8f3f51dd583c60b
jordangm94 <jordan.guerrero.martinez@gmail.com>
2022-10-13 17:56:14 +0000

    Implemented AJAX using jquery to make an asynchronous get request to /tweets, that way we can import tweets from the database. Removed hardcoded data and es linted file

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,32 +5,7 @@
  */
 // This is practice data, but the information points in this object are what we will pass into our tweet template below.
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
-// CRUCIAL Note: All functions and function call must be placed within document ready. Functions cannot work with DOM objects if page not loaded. 
+// CRUCIAL Note: All functions and function call must be placed within document ready. Functions cannot work with DOM objects if page not loaded.
 $(document).ready(function() {
   const renderTweets = function(tweets) {
     //Loops through array of tweet objects, each value will be an individual tweet object with user information.
@@ -39,12 +14,12 @@ $(document).ready(function() {
       const $tweet = createTweetElement(value);
       // takes return value and appends it to the tweets container
       $('.tweets-timeline').append($tweet);
-    };
-  }
+    }
+  };
   //This function creates a new tweet element by taking the data passed to it from above and passing the data points into specific area of the tweet template, using template literals.
-  //We took this HTML straight from our initial index.html file and put it here, that way we can pass any tweets information and they can be made to appear as the tweet template. 
+  //We took this HTML straight from our initial index.html file and put it here, that way we can pass any tweets information and they can be made to appear as the tweet template.
   const createTweetElement = function(tweetdata) {
-  const element = `
+    const element = `
   <article class="tweet-box">
   <header class="tweet-header">
     <div class="tweet-userphoto-username">
@@ -60,7 +35,7 @@ $(document).ready(function() {
       </p>
     </div>
     <footer class="tweet-footer">
-      <div class="day-counter">${tweetdata.created_at}</div>
+      <div class="day-counter">${timeago.format(tweetdata.created_at)}</div>
       <div class="social-icons">
         <i class="fa-solid fa-flag">  </i>
         <i class="fa-solid fa-heart">  </i>
@@ -70,27 +45,35 @@ $(document).ready(function() {
   </header>
   </article>
   `;
-  return element;
-  }
-  renderTweets(data);
+    return element;
+  };
 
   //The next line uses JQuery to target the tweet submit form, listen for submit event using .submit().
   $("#submit-tweet-form").submit(function(event) {
     //This next line prevents the submitting from making a post request to /tweets.
     event.preventDefault();
     //Next we are going to seralize the data so that it is "encoded" into a string that the browser can understand.
-    const formData= $(this).serialize();
+    const formData = $(this).serialize();
     //Next we use AJAX, we pass it the method, form data and which URL we are making this post request to.
     $.ajax({
       method: "POST",
       data: formData,
       url: "/tweets"
       //Using Ajax allows us to send the tweet out asynchronously; hence, we do not need a page refresh!
-    })
+    });
   });
-})
 
-// $("#submit-tweet").submit(functin(event) {
-
-// })
-
+  //With this function we are using AJAX to make a GET request to/tweets asynchronously so we will not need a page refresh.
+  //Once we get the /tweets page, we will receive the tweets in an array, we pass those tweets(response) into our renderTweet function. 
+  const loadTweets = function() {
+    $.ajax({
+      method: 'GET',
+      dataType: "json",
+      url: "/tweets",
+      success: function(response) {
+        renderTweets(response);
+      }
+    });
+  };
+  loadTweets();
+});
